9cc3331d14a0deee252603db854014b6ed1ac698
Tai Jing Shen <taijingshen@gmail.com>
2025-03-18 11:55:43 +0800

    clean up pdf_rag

--- src/agno_agents/pdf_rag.py
+++ src/agno_agents/pdf_rag.py
@@ -12,18 +12,29 @@ from agno.models.groq import Groq
 from dotenv import load_dotenv
 import os
 
+# ======================================================== START: TO SET THE VARIABLES  ========================================================
 # Load environment variables from .env file
 load_dotenv()
 os.environ["OPENAI_API_KEY"]=os.getenv("OPENAI_API_KEY")
 os.environ["GROQ_API_KEY"] = os.getenv("GROQ_API_KEY")
 
 # define pdf rag agent storage file & storage table name
-agent_storage_file: str = "tmp/pdf_rag_agent.db"
-table_name_agent: str = "AI-ACT-pdf_rag_agent"
+agent_storage_file: str = "tmp/pdf_rag.db"
+table_name_agent: str = "HBS-pdf_rag_agent"
 
 # Use persistent storage for ChromaDB
-collection_name="AI-ACT"
-chroma_db_path="./chromadb_data",  # Set storage location ./chromadb_data is at current working directory
+collection_name="HBS"
+chroma_db_path="./chromadb_data"
+
+# PDF urls 
+pdf_urls=["https://raw.githubusercontent.com/sciencenerd880/LLM-DBT/main/data/pdfs/hbs_opportunities.pdf",
+          "https://raw.githubusercontent.com/sciencenerd880/LLM-DBT/main/data/pdfs/hbs_pitchdeck_sample.pdf"
+          ]
+
+# https://github.com/sciencenerd880/LLM-DBT/tree/main/data/pdfs
+
+# ======================================================== END: TO SET THE VARIABLES  ==========================================================
+
 vector_db = ChromaDb(collection=collection_name, 
                     path= chroma_db_path,
                     persistent_client=True   # Enable persistence
@@ -31,14 +42,15 @@ vector_db = ChromaDb(collection=collection_name,
 
 # Defines the knowledge base
 knowledge_base = PDFUrlKnowledgeBase(
-    urls=["https://raw.githubusercontent.com/sciencenerd880/LLM-DBT/main/src/multi_agents/The-AI-Act.pdf"],
+    urls=pdf_urls,
     vector_db=vector_db, 
-)
+    )   
 
 def pdf_agent(user: str = "user", groq_model:str ="deepseek-r1-distill-llama-70b"):
     groq_agent = Agent(
         model=Groq(id=groq_model),
-        storage=SqliteAgentStorage(table_name=table_name_agent, db_file=agent_storage_file),
+        storage=SqliteAgentStorage(table_name=table_name_agent, 
+                                   db_file=agent_storage_file),
         show_tool_calls=True,
         debug_mode=True,
         knowledge=knowledge_base
